f2224ab02ac714e8118576610dcae9812fa1457e
joshualholland <joshualholland@gmail.com>
2018-12-04 15:24:48 -0600

    working on modal

--- client/app.js
+++ client/app.js
@@ -1,4 +1,30 @@
 
+const displayChirps = (data) => {
+    let chirpArray = Object.keys(data).map((key) => {
+        return {
+            id: key,
+            user: data[key].user,
+            chirp: data[key].chirp
+        }
+    })
+
+    chirpArray.pop();
+    chirpArray.reverse();
+    chirpArray.forEach(chirp => {
+        let card = [
+            `<div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">${chirp.user}</h5>
+                    <p class="card-text">${chirp.chirp}</p>
+                    <h6 class="card-subtitle mb-2 text-muted">Chirp: ${chirp.id}</h6>
+                    <button type="button" class="btn btn-danger btn-sm" id="delete-chirp" onclick="deleteChirp(${chirp.id})">X</button>
+                    <button type="button" class="btn btn-secondary btn-sm" id="edit-chirp" data-toggle="modal" data-target="#chirpModal">Edit</button>
+                </div>
+            </div>`
+        ]
+        $('.chirps').append(card)
+    })
+}
 
 $(document).ready(() => {
     $.ajax({
@@ -6,28 +32,10 @@ $(document).ready(() => {
         url: '/api/chirps',
         dataType: 'html'
     }).then(res => JSON.parse(res))
-        .then(data => {
-            Object.keys(data).map((key) => {
-                let chirpArray = (data[key])
-                if(key === 'nextid') return;
-                console.log(chirpArray)
-                let card = [
-                    `<div class="card">
-                        <div class="card-body">
-                            <button type="button" class="btn btn-danger">X</button>
-                            <h5 class="card-title">${chirpArray.user}</h5>
-                            <p class="card-text">${chirpArray.chirp}</p>
-                            <footer class="blockquote-footer">${key}</footer>
-                        </div>
-                    </div>`
-                ]
-                $('.chirps').append(card)
-            })
-        })
-
+        .then(data => displayChirps(data))
 });
 
-$('button').click(() => {
+$('#newChirp').click(() => {
     let user = $('#user').val();
     let chirp = $('#chirp').val();
     let chirpObject = {
@@ -40,16 +48,33 @@ $('button').click(() => {
         data: chirpObject,
         dataType: 'html'
     }).then(res => JSON.parse(res))
-    .then(data => {
-        let card = [
-            `<div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">${data.chirpObject.user}</h5>
-                    <p class="card-text">${data.chirpObject.chirp}</p>
-                </div>
-            </div>`
-        ]
-        $('.chirps').append(card)
-    })
+        .then(data => displayChirps(data))
+    location.reload();
 })
+
+const deleteChirp = (id) => {
+    $.ajax({
+        type: 'DELETE',
+        url: `/api/chirps/${id}`
+    })
+        .then(res => JSON.parse(res))
+        .then(data => displayChirps(data))
+    location.reload();
+};
+
+$('#confirm-edit').click(() => {
+    let user = $('#user').val();
+    let chirp = $('#editText').val();
+    let chirpObject = {
+        user,
+        chirp
+    }
+    $.ajax({
+        type: 'PUT',
+        url: `/api/chirps/`,
+        data: chirpObject
+    })
+        .then(res => JSON.parse(res))
+        .then(data => displayChirps(data))
+});
 
