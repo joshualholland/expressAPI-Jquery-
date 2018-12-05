56c5d7f2662058ad21925d84de702e216994221b
joshualholland <joshualholland@gmail.com>
2018-12-05 14:14:47 -0600

    fin

--- client/app.js
+++ client/app.js
@@ -18,7 +18,7 @@ const displayChirps = (data) => {
                     <p class="card-text">${chirp.chirp}</p>
                     <h6 class="card-subtitle mb-2 text-muted editChirp" id="${chirp.id}">${chirp.id}</h6>
                     <button type="button" class="btn btn-danger btn-sm" id="delete-chirp" onclick="deleteChirp(${chirp.id})">X</button>
-                    <button type="button" class="btn btn-secondary btn-sm" id="edit-chirp" data-toggle="modal" data-target="#chirpModal">Edit</button>
+                    <button type="button" class="btn btn-secondary btn-sm" id="edit-chirp" data-toggle="modal" data-target="#chirpModal" onclick="editChirp(${chirp.id})">Edit</button>
                 </div>
             </div>`
         ]
@@ -62,21 +62,26 @@ const deleteChirp = (id) => {
     location.reload();
 };
 
-$('#confirm-edit').click(() => {
-    let id = $('.editChirp').attr("id");
-    let user = $('#chirpUser').text();
-    let chirp = $('#editText').val();
-    let chirpObject = {
-        id,
-        user,
-        chirp
-    }
-    $.ajax({
-        type: 'PUT',
-        url: `/api/chirps/${id}`,
-        data: chirpObject,
-    })
-        .then(res => JSON.parse(res))
-        .then(data => displayChirps(data))
-});
+const editChirp = (id) => {
+    $('#exampleModalLabel').text(`Edit Chirp: ${id}`);
+    $.get(`/api/chirps/${id}`)
+        .then(data => {
+            $('#editText').val(data.chirp);
+            $('#confirm-edit').click(() => {
+                let chirpObject = {
+                    id: id,
+                    user: data.user,
+                    chirp: $('#editText').val()
+                }
+                $.ajax({
+                    type: 'PUT',
+                    url: `/api/chirps/${id}`,
+                    data: chirpObject,
+                })
+                    .then(location.reload())
+            });
+        })
+}
+
+
 
